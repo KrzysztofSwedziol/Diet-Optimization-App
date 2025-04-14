af4fc8ca6b2b5b92eeae1784c3afc5d21da1bfd1
Jeremy <barycki@student.agh.edu.pl>
2025-04-14 22:25:38 +0200

    delete errors

--- dyeti-backend/src/main/java/ki/agh/dyeti/DyetiBackendApplication.java
+++ dyeti-backend/src/main/java/ki/agh/dyeti/DyetiBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DyetiBackendApplication {
-edfsdfsdf
+
 	public static void main(String[] args) {
 		SpringApplication.run(DyetiBackendApplication.class, args);
 	}
