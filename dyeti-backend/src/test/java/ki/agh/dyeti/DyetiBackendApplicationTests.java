9b54994429f8680942f9fb899f0249b6db7d4859
Jeremy <barycki@student.agh.edu.pl>
2025-04-14 17:43:43 +0200

    spring init

---
+++ dyeti-backend/src/test/java/ki/agh/dyeti/DyetiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ki.agh.dyeti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DyetiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
