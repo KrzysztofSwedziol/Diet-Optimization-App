7d30f05eae81db2eb51181b0e3c44cd32dcc3039
Jeremy <barycki@student.agh.edu.pl>
2025-04-14 22:40:08 +0200

    actions test

--- dyeti-backend/src/main/java/ki/agh/dyeti/DyetiBackendApplication.java
+++ dyeti-backend/src/main/java/ki/agh/dyeti/DyetiBackendApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DyetiBackendApplication {
-fdsgf
+
 	public static void main(String[] args) {
 		SpringApplication.run(DyetiBackendApplication.class, args);
 	}
-dfgdfgdf
+
 }
-fvdsf
