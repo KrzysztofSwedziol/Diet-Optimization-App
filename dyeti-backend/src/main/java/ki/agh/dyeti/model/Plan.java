7daef32978c080606226f58982a67deef698b4ed
Jeremy <barycki@student.agh.edu.pl>
2025-05-04 17:47:52 +0200

    [DIET-51-Prototyp-Bazy-Danych-Spring] folder for model changed (mistake) plans added

---
+++ dyeti-backend/src/main/java/ki/agh/dyeti/model/Plan.java
@@ -0,0 +1,30 @@
+package ki.agh.dyeti.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "plans")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Plan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String planName;
+    private java.time.LocalDateTime planDate;
+    private Integer energyReq;
+    private Integer proteinReq;
+    private Integer fatReq;
+    private Integer carbsReq;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
