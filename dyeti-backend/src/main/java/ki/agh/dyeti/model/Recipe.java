67d0b05883ab2e1bab4b4f334ec0901f53217cf4
Jeremy <barycki@student.agh.edu.pl>
2025-05-04 17:51:47 +0200

    [DIET-51-Prototyp-Bazy-Danych-Spring] Recipe model added

---
+++ dyeti-backend/src/main/java/ki/agh/dyeti/model/Recipe.java
@@ -0,0 +1,27 @@
+package ki.agh.dyeti.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "recipes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String recipeName;
+    private String description;
+    private String steps;
+
+    @ManyToOne
+    @JoinColumn(name = "creator_id")
+    private User creator;
+}
