0ad44eb7c4f14dd6a82cba0584df7e8305db4c03
Jeremy <barycki@student.agh.edu.pl>
2025-05-05 23:46:25 +0200

    [DIET-55-auth-controller] UserDTO changed to return all important information about user

--- dyeti-backend/src/main/java/ki/agh/dyeti/dto/UserDTO.java
+++ dyeti-backend/src/main/java/ki/agh/dyeti/dto/UserDTO.java
@@ -2,18 +2,38 @@ package ki.agh.dyeti.dto;
 
 import ki.agh.dyeti.model.Role;
 import ki.agh.dyeti.model.User;
+import ki.agh.dyeti.model.util.Gender;
+import lombok.Builder;
 
-public record UserDTO(Long id, String username, String email) {
-
+@Builder
+public record UserDTO(
+        Long id,
+        String username,
+        String email,
+        Integer age,
+        Gender gender,
+        Integer height,
+        Integer weight,
+        Integer energyReq,
+        Integer proteinReq,
+        Integer carbsReq,
+        Integer fatReq,
+        Role role
+) {
     public static UserDTO fromUser(User user) {
-        return new UserDTO(user.getId(), user.getUsername(), user.getEmail());
-    }
-
-    public static User toUser(UserDTO userDTO, Role role) {
-        return User.builder()
-                .email(userDTO.email())
-                .username(userDTO.username())
-                .role(role)
+        return UserDTO.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .age(user.getAge())
+                .gender(user.getGender())
+                .height(user.getHeight())
+                .weight(user.getWeight())
+                .energyReq(user.getEnergyReq())
+                .proteinReq(user.getProteinReq())
+                .carbsReq(user.getCarbsReq())
+                .fatReq(user.getFatReq())
+                .role(user.getRole())
                 .build();
     }
 }
