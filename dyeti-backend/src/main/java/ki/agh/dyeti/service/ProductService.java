babae5e571c3c0a73c40fd2e5e9c1bbe8807622c
Jeremy <barycki@student.agh.edu.pl>
2025-05-16 14:26:29 +0200

    [DIET-77-project-structure-mvc] join table respository added to services

--- dyeti-backend/src/main/java/ki/agh/dyeti/service/ProductService.java
+++ dyeti-backend/src/main/java/ki/agh/dyeti/service/ProductService.java
@@ -1,13 +1,22 @@
 package ki.agh.dyeti.service;
 
+import ki.agh.dyeti.repository.PlansProductsRepository;
 import ki.agh.dyeti.repository.ProductRepository;
+import ki.agh.dyeti.repository.RecipesProductsRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final PlansProductsRepository plansProductsRepository;
+    private final RecipesProductsRepository recipesProductsRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(
+            ProductRepository productRepository,
+            PlansProductsRepository plansProductsRepository,
+            RecipesProductsRepository recipesProductsRepository) {
         this.productRepository = productRepository;
+        this.plansProductsRepository = plansProductsRepository;
+        this.recipesProductsRepository = recipesProductsRepository;
     }
 }
--- dyeti-backend/src/main/java/ki/agh/dyeti/service/RecipeService.java
+++ dyeti-backend/src/main/java/ki/agh/dyeti/service/RecipeService.java
@@ -1,13 +1,22 @@
 package ki.agh.dyeti.service;
 
+import ki.agh.dyeti.repository.PlansRecipesRepository;
 import ki.agh.dyeti.repository.RecipeRepository;
+import ki.agh.dyeti.repository.RecipesProductsRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RecipeService {
     private final RecipeRepository recipeRepository;
+    private final RecipesProductsRepository recipesProductsRepository;
+    private final PlansRecipesRepository plansRecipesRepository;
 
-    public RecipeService(RecipeRepository recipeRepository) {
+    public RecipeService(
+            RecipeRepository recipeRepository,
+            RecipesProductsRepository recipesProductsRepository,
+            PlansRecipesRepository plansRecipesRepository) {
         this.recipeRepository = recipeRepository;
+        this.recipesProductsRepository = recipesProductsRepository;
+        this.plansRecipesRepository = plansRecipesRepository;
     }
 }
