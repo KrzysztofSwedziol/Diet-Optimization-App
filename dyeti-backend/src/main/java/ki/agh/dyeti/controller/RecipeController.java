fba2abab48ec7bca88be20772e557fe63842b49d
Jeremy <barycki@student.agh.edu.pl>
2025-05-16 13:59:40 +0200

    [DIET-77-project-structure-mvc] all controllers by far

---
+++ dyeti-backend/src/main/java/ki/agh/dyeti/controller/RecipeController.java
@@ -0,0 +1,8 @@
+package ki.agh.dyeti.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/recipe")
+public class RecipeController {}
