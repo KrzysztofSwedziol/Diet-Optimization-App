8481b06a7e67186f12cb6b09b52ebddc704b26c8
Jeremy <barycki@student.agh.edu.pl>
2025-05-19 22:09:16 +0200

    fixed

--- dyeti-backend/src/main/java/ki/agh/dyeti/exception/UserAlreadyHasElevatedRoleException.java
+++ dyeti-backend/src/main/java/ki/agh/dyeti/exception/UserAlreadyHasElevatedRoleException.java
@@ -1,7 +1,3 @@
 package ki.agh.dyeti.exception;
 
-public class UserAlreadyHasElevatedRoleException extends RuntimeException {
-    public UserAlreadyHasElevatedRoleException(Long userId, String currentRole) {
-        super("User with ID " + userId + " already has role " + currentRole);
-    }
-}
+public class UserAlreadyHasElevatedRoleException {}
