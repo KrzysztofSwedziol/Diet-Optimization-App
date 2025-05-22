35f261c424c93464f0c53b73b01335fa50d4988a
Jeremy <barycki@student.agh.edu.pl>
2025-05-22 20:06:24 +0200

    rules of hooks working

--- dyeti-frontend/eslint.config.js
+++ dyeti-frontend/eslint.config.js
@@ -2,12 +2,15 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import prettier from "eslint-config-prettier"
+import reactHooks from 'eslint-plugin-react-hooks';
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: { js },
+
     extends: ["js/recommended"],
   },
   {
@@ -16,5 +19,7 @@ export default defineConfig([
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat["jsx-runtime"]
+  pluginReact.configs.flat["jsx-runtime"],
+  reactHooks.configs['recommended-latest'],
+  prettier,
 ]);
