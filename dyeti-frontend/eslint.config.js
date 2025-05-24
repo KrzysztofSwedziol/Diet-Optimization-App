f90e5fd4044942e7a9c9fa790484ed617154c6dc
Jeremy <barycki@student.agh.edu.pl>
2025-05-24 15:28:16 +0200

    all working by far AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaa

--- dyeti-frontend/eslint.config.js
+++ dyeti-frontend/eslint.config.js
@@ -4,22 +4,82 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import prettier from "eslint-config-prettier"
 import reactHooks from 'eslint-plugin-react-hooks';
-import { defineConfig } from "eslint/config";
+import importPlugin from 'eslint-plugin-import';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: { js },
+  // {
+  //   files: ["**/*.{js,mjs,cjs,mts,cts,jsx}"],
+  //   plugins: { js },
+  //   extends: ["js/recommended"],
+  // },
+  // {
+  //   files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  //   languageOptions: { globals: globals.browser },
+  // },
+  globalIgnores(["eslint.config.js"]),
 
-    extends: ["js/recommended"],
+  {
+    name: "JavaScript + React Rules",
+    files: ["**/*.{js,mjs,cjs,mts,cts,jsx}"],
+    plugins: {
+      react: pluginReact,
+      "react-hooks": reactHooks,
+      import: importPlugin,
+    },
+    extends:[
+      js.configs.recommended,
+    ],
+    rules: {
+      ...reactHooks.configs['recommended-latest'].rules,
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReact.configs['jsx-runtime'].rules,
+      ...importPlugin.configs.recommended.rules,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    languageOptions: { globals: globals.browser },
+    name: "TypeScript + React Rules",
+    files: ['**/*.{tsx,ts}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+          modules: true,
+        },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "typescript-eslint": tseslint,
+      react: pluginReact,
+      "react-hooks": reactHooks,
+      import: importPlugin,
+    },
+    extends:[
+      tseslint.configs.recommended
+    ],
+    rules: {
+      ...reactHooks.configs['recommended-latest'].rules,
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReact.configs['jsx-runtime'].rules,
+      ...importPlugin.configs.recommended.rules,
+    },
+
+
   },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat["jsx-runtime"],
-  reactHooks.configs['recommended-latest'],
-  prettier,
+  prettier
+  // tseslint.config(
+  //   { files: ['**/*.{tsx,ts}'] },
+  //   js.configs.recommended,
+  // ),
+
 ]);
