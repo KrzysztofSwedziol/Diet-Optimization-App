9a5bbf7971e6fa88d6179ba5fb3e05ba9ce563c3
Eryk <olejarzeryk@gmail.com>
2025-06-21 21:37:50 +0200

    add initial units

---
+++ dyeti-backend/src/main/java/ki/agh/dyeti/config/DataInitializer.java
@@ -0,0 +1,28 @@
+package ki.agh.dyeti.config;
+
+import ki.agh.dyeti.model.Unit;
+import ki.agh.dyeti.repository.UnitRepository;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    public ApplicationRunner initializeUnits(UnitRepository unitRepository) {
+        return args -> {
+            List<Unit> defaultUnits = List.of(
+                new Unit(null, "gram", "g"),
+                new Unit(null, "milliliter", "ml"),
+                new Unit(null, "piece", "pc")
+            );
+
+            defaultUnits.stream()
+                .filter(unit -> !unitRepository.existsByName(unit.getName()))
+                .forEach(unitRepository::save);
+        };
+    }
+}
