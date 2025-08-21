831ae0b945b795e17688e91388ce03b76efec7a8
Eryk666 <olejarzeryk@gmail.com>
2025-08-21 22:58:01 +0200

    [DIET-109-NavBar-SideBar] feature: add main routes and use app layout

--- dyeti-frontend/src/App.tsx
+++ dyeti-frontend/src/App.tsx
@@ -1,12 +1,17 @@
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Themes from './pages/Themes.tsx';
+import AppLayout from './components/AppLayout/AppLayout.tsx';
+import { Home, Plans, Products, Themes } from './pages/index.ts';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={'/themes'} element={<Themes />} />
+        <Route path={'/'} element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/plans" element={<Plans />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/themes" element={<Themes />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
