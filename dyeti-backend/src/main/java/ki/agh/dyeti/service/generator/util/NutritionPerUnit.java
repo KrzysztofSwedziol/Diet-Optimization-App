021fd56c4e2f695b54e4d8f8012c7313a980f5f3
Eryk <olejarzeryk@gmail.com>
2025-09-13 20:41:40 +0200

    refactor: extract magic numbers

--- dyeti-backend/src/main/java/ki/agh/dyeti/service/generator/util/NutritionPerUnit.java
+++ dyeti-backend/src/main/java/ki/agh/dyeti/service/generator/util/NutritionPerUnit.java
@@ -6,6 +6,8 @@ import lombok.Data;
 @Data
 public class NutritionPerUnit {
 
+    private static final double HUNDRED_GRAMS = 100.0;
+
     private final double kcal;
     private final double protein;
     private final double carbs;
@@ -13,9 +15,9 @@ public class NutritionPerUnit {
 
     public NutritionPerUnit(Product product) {
         double grams = product.getGramsPerUnit();
-        this.kcal = grams * product.getKcal100g() / 100.0;
-        this.protein = grams * product.getProtein100g() / 100.0;
-        this.carbs = grams * product.getCarbs100g() / 100.0;
-        this.fats = grams * product.getFat100g() / 100.0;
+        this.kcal = grams * product.getKcal100g() / HUNDRED_GRAMS;
+        this.protein = grams * product.getProtein100g() / HUNDRED_GRAMS;
+        this.carbs = grams * product.getCarbs100g() / HUNDRED_GRAMS;
+        this.fats = grams * product.getFat100g() / HUNDRED_GRAMS;
     }
 }
