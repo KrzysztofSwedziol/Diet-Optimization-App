a3cd477f0cfca1484cf2a38ecc0bf8b0d7803a3a
Jeremy <barycki@student.agh.edu.pl>
2025-09-17 13:17:35 +0200

    [DIET-110-Products-page] trying to rerun backendjob to merge

--- dyeti-backend/src/main/java/ki/agh/dyeti/controller/AdminController.java
+++ dyeti-backend/src/main/java/ki/agh/dyeti/controller/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+// idk
 
 @RestController
 @RequestMapping("/admin")
