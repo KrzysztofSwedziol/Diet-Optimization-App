bc3cf946ba9dac3b3642d3eeb3522da2a15a02f3
Jeremy <barycki@student.agh.edu.pl>
2025-10-28 10:14:22 +0100

    [DIET-116-Account-page] more magic numbers error fixed

--- dyeti-backend/src/main/java/ki/agh/dyeti/dto/request/ChangePasswordRequest.java
+++ dyeti-backend/src/main/java/ki/agh/dyeti/dto/request/ChangePasswordRequest.java
@@ -3,4 +3,7 @@ package ki.agh.dyeti.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record ChangePasswordRequest(@NotBlank String currentPassword, @NotBlank @Size(min = 8) String newPassword) {}
+public record ChangePasswordRequest(
+        @NotBlank String currentPassword, @NotBlank @Size(min = MIN_PASSWORD) String newPassword) {
+    private static final int MIN_PASSWORD = 8;
+}
