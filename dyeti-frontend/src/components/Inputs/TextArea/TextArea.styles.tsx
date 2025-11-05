e03f40ce88dc880e694641e0591c4579e57f4f54
Eryk <olejarzeryk@gmail.com>
2025-11-05 20:12:59 +0100

    [feat/basic-flow] styling: text area

--- dyeti-frontend/src/components/Inputs/TextArea/TextArea.styles.tsx
+++ dyeti-frontend/src/components/Inputs/TextArea/TextArea.styles.tsx
@@ -5,23 +5,20 @@ export const InputTextArea = styled.textarea<{ hasError?: boolean }>`
   margin-bottom: 8px;
   min-height: 200px;
   color: ${({ theme }) => theme.colors.neutrals[900]};
-  background: ${({ theme }) => theme.colors.primary[100]};
+  background: ${({ theme }) => theme.colors.background};
   border: ${({ theme }) => theme.borderWidth.normal} solid
     ${({ hasError, theme }) => (hasError ? theme.colors.danger[500] : theme.colors.neutrals[600])};
   border-radius: ${({ theme }) => theme.borderRadius.md};
   font-size: ${({ theme }) => theme.typography.fontSize.md};
   caret-color: ${({ theme }) => theme.colors.neutrals[700]};
   font-family: ${({ theme }) => theme.typography.fontFamily};
+
   &::placeholder {
     color: ${({ theme }) => theme.colors.neutrals[400]};
   }
+
   &:focus {
     outline: none;
     border-color: ${({ theme }) => theme.colors.primary[700]};
-    box-shadow: 0 0 0 ${({ theme }) => theme.borderWidth.thin} ${({ theme }) => theme.colors.primary[700]};
-  }
-
-  &:hover {
-    box-shadow: ${({ theme }) => theme.shadows.md};
   }
 `;
