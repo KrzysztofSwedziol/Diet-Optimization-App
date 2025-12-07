1944fac58657211beb52f216568c233e1cc4d595
Eryk666 <olejarzeryk@gmail.com>
2025-12-07 17:07:24 +0100

    [DIET-152-Plan-generation-improvements] refactor: plan api

--- dyeti-frontend/src/api/plan/plan.mutations.ts
+++ dyeti-frontend/src/api/plan/plan.mutations.ts
@@ -1,17 +1,18 @@
 import { Plan } from '@/types';
 import { apiRequest } from '../axios';
-import { HttpMethod } from '../types';
+import { HttpMethod, PlanGenerationRequest } from '../types';
+
+const generatePlan = async (data: PlanGenerationRequest) => {
+  return await apiRequest<string>(HttpMethod.POST, '/api/plans', data);
+};
 
 const deletePlan = async (planId: number) => {
-  try {
-    const response = await apiRequest<Plan>(HttpMethod.DELETE, `/api/plans/${planId}`);
-    return response;
-  } catch (error) {
-    throw error;
-  }
+  const response = await apiRequest<Plan>(HttpMethod.DELETE, `/api/plans/${planId}`);
+  return response;
 };
 
 const mutations = {
+  generatePlan,
   deletePlan,
 };
 
